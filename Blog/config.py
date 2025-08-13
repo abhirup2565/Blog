376f0d5509715c47eba6c94999a96478a39fa05a
abhirup2565 <abhirup2565@gmail.com>
2025-08-13 19:02:45 +0530

    supadb

--- Blog/config.py
+++ Blog/config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'cantcatchme'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+    SQLALCHEMY_DATABASE_URI="postgresql://postgres.lujhdjtaoendcrhgufjb:gdJTVXvOnXlTPDLZ@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
     UPLOAD_FOLDER='static/images/profile'
     UPLOAD_CONTENT_FOLDER='static/images/content'
